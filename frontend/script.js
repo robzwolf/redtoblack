d78b747e1b0acd8e902ab61cc94a1f8f404b8c78
Robbie Jakob-Whitworth <robzwolf@gmail.com>
2017-04-01 19:28:15 +0100

    Added methods to draw food/leisure progress bars in script.js

--- frontend/script.js
+++ frontend/script.js
@@ -21,20 +21,51 @@ var getLatestData = function()
 
 };
 
+/* Draw the leisure progress bar with a value on a scale from 0 to 1 */
+var drawLeisureBar = function(value)
+{
+  var leisurebar = new ProgressBar.Line('#progress-leisure', {
+        color: '#82CCA9',
+        duration: 800,
+        easing: 'easeInOut'
+  });
+  leisurebar.animate(value);
+}
+
+
+
+/* Draw the food progress bar with a value on a scale from 0 to 1 */
+var drawFoodBar = function(value)
+{
+  var foodbar = new ProgressBar.Line('#progress-food', {
+        color: '#82CCA9',
+        duration: 800,
+        easing: 'easeInOut'
+  });
+  foodbar.animate(value);
+}
+
 
 $(document).ready(function(){
-  var leisurebar = new ProgressBar.Line('#progress-leisure', {
-        color: '#4dd',
-        duration: 800,
-        easing: 'easeInOut'
-  });
-  var foodbar = new ProgressBar.Line('#progress-food', {
-        color: '#4dd',
-        duration: 800,
-        easing: 'easeInOut'
-  });
-  leisurebar.animate(1);
-  setTimeout(function(){
-    foodbar.animate(0.5);
-  },400);
+
+  // var leisurebar = new ProgressBar.Line('#progress-leisure', {
+  //       color: '#82CCA9',
+  //       duration: 800,
+  //       easing: 'easeInOut'
+  // });
+  //
+  // var foodbar = new ProgressBar.Line('#progress-food', {
+  //       color: '#82CCA9',
+  //       duration: 800,
+  //       easing: 'easeInOut'
+  // });
+  //
+  // leisurebar.animate(1);
+  // setTimeout(function(){
+  //   foodbar.animate(0.5);
+  // },400);
+
+  drawLeisureBar(1);
+  drawFoodBar(0.5);
+
 });
