77a09e36b7f6dec385c2248644eb57a19fc4f428
Robbie Jakob-Whitworth <robzwolf@gmail.com>
2017-04-02 08:06:35 +0100

    data.js now gets JSON from api.php

--- frontend/data.js
+++ frontend/data.js
@@ -1,34 +1,45 @@
 // data.js
 // Pulls data from the API and draws it on the page
 
+json = null;
+
+$.getJSON("http://community.dur.ac.uk/vivek.a.mehta/redtoblack/api.php?action=weeklySpend&userID=1",function(data){
+  json = data;
+});
+
 // THE FOLLOWING FUNCTIONS RETURN DUMMY DATA
 
 var getLeisureSpend = function()
 {
-  return {
-    "limit": 100,
-    "spend": 70
-  }
+  return json.leisure;
+  // return {
+  //   "limit": 100,
+  //   "spend": 70
+  // }
 }
 
 var getFoodSpend = function()
 {
-  return {
-    "limit": 100,
-    "spend": 20
-  }
+  return json.food;
+  // return {
+  //   "limit": 100,
+  //   "spend": 20
+  // }
 }
 
 var getTravelSpend = function()
 {
-  return {
-    "limit": 20,
-    "spend": 10
-  }
+  return json.travel;
+  // return {
+  //   "limit": 20,
+  //   "spend": 10
+  // }
 }
 
 var getBillsSpend = function()
 {
+  // return json.bills;
+  console.log("bills",json.bills);
   return {
     "phone": {
       "expectedOut": 25,
